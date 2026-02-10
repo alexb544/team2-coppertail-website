19c2d85c8f99fe8cd1b9919e787fb4fc8978dddc
Alex Blake <alexblake544@gmail.com>
2026-02-10 13:10:43 -0800

    modified admin panel for testing accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Profile, Dog
 
-# Register your models here.
+try:
+    admin.site.unregister(Group)
+except admin.sites.NotRegistered:
+    pass
+
+class DogInline(admin.TabularInline):
+    model = Dog
+    extra = 0
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "user", 
+        "phone_number"
+    )
+    search_fields = (
+        "user__username", 
+        "user__first_name", 
+        "user__last_name", 
+        "user__email"
+    )
+    inlines = [DogInline]
+
+
+@admin.register(Dog)
+class DogAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "breed", "age")
+    search_fields = (
+        "name",
+        "breed", 
+        "owner__user__first_name"
+        "owner__user__last_name"
+    )
+    list_filter = ("breed",)
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 
+
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20, blank=True)
