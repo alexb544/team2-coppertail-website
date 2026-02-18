ce657c1a92a84e14b369423f94292b9f7217076e
Melanie Fruciano <mfruciano@mocs.flsouthern.edu>
2026-02-18 02:54:34 -0500

    created user login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
+#def login_view(request):
+   # return render(request, "accounts/login.html")
+
+
+# Custom login view using Django's built-in authentication
+class CustomLoginView(LoginView):
+    template_name = "accounts/login.html"  # points to the login template
+
