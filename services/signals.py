47ab4b75c6db905791b4865698c9fa74f27f06df
Matthew Greenblatt <mgreenblatt@mocs.flsouthern.edu>
2026-02-25 17:23:40 -0500

    All necessary files for services backend

---
+++ services/signals.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from accounts.models import Profile
+
+User = get_user_model()
+@receiver(post_save, sender=User)
+def create_user_services(sender, instance, created, **kwargs):
+    if created:
+        Services.object.create(user=instance)
