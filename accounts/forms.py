4e43521f39d61a65e55337373db407a0832bfb06
Matthew Greenblatt <mgreenblatt@mocs.flsouthern.edu>
2026-02-25 19:30:23 -0500

    Updated forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,22 +1,27 @@
 #Imports
 from django import forms
-from django.contrib.auth.models import User
-from .models import Profile
-
-#Form class for updating user information
-class UserUpdateForm(forms.ModelForm):
-    #Handles how the form connects to the user model
-    class Meta:
-        #Alerts that the User model is being updated
-        model = User
-        #Specifies what User fields are editable
-        fields = ['username', 'email']
+from .models import Profile, Dog
 
 #Form class for updating profile information
-class ProfileUpdateForm(forms.modelForm):
-    #Handles how the form connects to the profile model
+class ProfileUpdateForm(forms.ModelForm):
+    #Handles how the form connects to the Profile model
     class Meta:
         #Alerts that the Profile model is being updated
         model = Profile
         #Specifies what Profile fields are editable
-        fields = ['phone', 'profile_picture', 'dog_name', 'dog_breed', 'dog_age']
+        fields = ["phone_number", "address"]
+
+        widgets = {"phone_number": forms.TextInput(ttrs={"class": "form-control"}), "address": forms.TextInput(attrs={"class": "form-control"},)}
+
+#Form class for updating dog information
+class DogUpdateForm(forms.modelForm):
+    #Handles how the form connects to the Dog model
+    class Meta:
+        #Alerts that the Dog model is being updated
+        model = Profile
+        #Specifies what Dog fields are editable
+        fields = ["name", "breed", "age", "notes"]
+
+        widgets = {"name": forms.TextInput(attr={"class": "form-control"}), "breed": forms.TextInput(attrs={"class": "form-control"}), "age": forms.NumberInput(attrs={"class": "form-control"}), "notes": forms.Textarea(attrs={"rows": 2, "class": "form-control"}),}
+
+        DogFormSet = inlineformset_factory(Profile, Dog, form=DogUpdateForm, extra=0, can_delete=True)
